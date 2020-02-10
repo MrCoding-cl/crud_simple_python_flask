918f8c39acd3d00cf002936cf2b031bd2d50ac13
Gabriel <gbordesc@uft.edu>
2020-02-10 12:36:13 -0300

    Solo falta el edit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    done = db.Column(db.Boolean)
+
+@app.route('/')
+def home():
+    tasks=Task.query.all
+    tasks = Task.query.all()
+    return render_template('index.html', tasks = tasks)
+
+@app.route('/create-task', methods=['POST'])
+def create():
+    task = Task(content=request.form['content'], done= False)
+    db.session.add(task)
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/delete/<id>')
+def delete(id):
+    task=Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/edit/<id>')
+def edit(id):
+    task=Task.query.filter_by(id=int(id)).first()
+    db.session.commit()
+    return redirect("/")
+if __name__ == '__main__':
+    app.run(debug=True)
